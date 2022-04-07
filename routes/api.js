9cab476543486606da7c2c2e5114930a498b625d
Aurèle <aurele@spimed-ai.com>
2022-04-07 11:35:52 +0200

    fix

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 /** Récupération de la date d'aujourd'hui */
 let date_ob = new Date();
